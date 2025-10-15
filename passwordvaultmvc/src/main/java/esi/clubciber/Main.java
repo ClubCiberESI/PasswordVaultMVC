f84ef9004a5301ce28c01f3a5e13bc20c5ec1432
eromeroc97 <45209976+eromeroc97@users.noreply.github.com>
2025-10-15 10:47:17 +0200

    Add properties-based view selection and ConsoleView stub
    Main.java now loads view type from project.properties and selects between CLI and GUI (GUI not yet implemented). Added ConsoleView class as a stub for CLI interactions, removed empty CliView, and introduced project.properties to configure the view type.

--- passwordvaultmvc/src/main/java/esi/clubciber/Main.java
+++ passwordvaultmvc/src/main/java/esi/clubciber/Main.java
@@ -1,7 +1,31 @@
 package esi.clubciber;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Properties properties = new Properties();
+            InputStream input = Main.class.getClassLoader().getResourceAsStream("project.properties");
+            if (input != null) {
+                properties.load(input);
+                String viewType = properties.getProperty("view.type");
+                if ("Cli".equals(viewType)) {
+                    // TODO: Launch CliView
+                    System.out.println("Launching CLI View");
+                    // For now, just print
+                } else if ("Gui".equals(viewType)) {
+                    // TODO: Launch GuiView
+                    Throw new UnsupportedOperationException("GUI View not implemented yet");
+                } else {
+                    System.out.println("Unknown view type");
+                }
+            } else {
+                System.out.println("Properties file not found");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ passwordvaultmvc/src/main/java/esi/clubciber/view/ConsoleView.java
@@ -0,0 +1,32 @@
+package esi.clubciber.view;
+
+public class ConsoleView {
+    // TODO: Add field for Scanner
+    // private Scanner scanner;
+
+    // TODO: Constructor
+    public ConsoleView() {
+        // TODO: Initialize scanner
+    }
+
+    // TODO: Method to display menu
+    public void displayMenu() {
+        // TODO: Print menu options
+    }
+
+    // TODO: Method to get user input
+    public String getUserInput() {
+        // TODO: Read input from scanner
+        return null;
+    }
+
+    // TODO: Method to display password entries
+    public void displayPasswordEntries() {
+        // TODO: Print entries
+    }
+
+    // TODO: Method to display message
+    public void displayMessage(String message) {
+        // TODO: Print message
+    }
+}
